54f2393a803d9114b486e6b0e5b70f3e9396e7e4
Lantz <lantzshaw@163.com>
2020-11-02 13:58:57 +0800

    'ES6类'

--- app.js
+++ app.js
@@ -1,60 +1,16 @@
-function Point(x, y) {
-    this.x = x
-    this.y = y
+class Point {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
 
-    this.update = () => {
-            console.log('update')
-            console.log(this)
-        }
-        // 不能这样这声明
-        // function update() {
-        //     console.log('update')
-        // }
-}
-
-// 会被下面的代码覆盖
-Point.prototype.getPoint = function() {
-    console.log('x:', this.x, 'y:', this.y)
-}
-
-Point.prototype = {
     getLine() {
         console.log('getLine')
-    },
+    }
 
     getPoint() {
         console.log('getPoint')
     }
 }
 
-const p = new Point(12, 2)
-
-// 会报错
-// p.prototype.update()
-
-
-// false
-console.log(p.hasOwnProperty('getLine'))
-console.log(p.hasOwnProperty('update'))
-
-
-// {x: 12, y: 2}
-console.log(p)
-
-// Object
-console.log(typeof p)
-
-// Function
-console.log(typeof Point)
-
-// true
-console.log(p.constructor === Point.prototype.constructor)
-
-// ['getLine', 'getPoint']
-console.log(Object.keys(Point.prototype))
-
-console.log(Object.getOwnPropertyNames(Point.prototype))
-
-p.getLine()
-p.getPoint()
-p.update()
+const p = new Point(1, 12)
